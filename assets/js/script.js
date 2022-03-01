bb4fcaa7fd38b780b6d18d3057287494abcdb7fa
mcstewart76 <mcstewart76@gmail.com>
2022-02-28 21:05:46 -0500

    Added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,29 +2,29 @@ var apikey = "a42e069d2d9555e36ce33bf63f6cfdae";
 var searchCityList = [];
 var city = document.getElementById('searchCity');
 var searchButton = document.getElementById('search');
-searchButton.addEventListener('click',searchFunction);
+searchButton.addEventListener('click', searchFunction);
 var Uv = document.getElementById('uv-index');
 var clearHistory = document.getElementById('clearHistory')
-clearHistory.addEventListener('click',function(){$('#searchHistory').empty()})
+clearHistory.addEventListener('click', function () { $('#searchHistory').empty() })
 var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-      };
+    method: 'GET',
+    redirect: 'follow'
+};
 
-function searchFunction(){
+//event listener to trigger weather function
+function searchFunction() {
     getWeather(city.value)
-    
-    //console.log(city.value)
 }
 
-function getWeather(cityName){
-    
-      
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}&units=imperial&lang=en&mode=json`, requestOptions)
-        .then(response => {return response.json()})
+//function to gather weather info from api using city name
+function getWeather(cityName) {
+
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}&units=imperial&lang=en&mode=json`, requestOptions)
+        .then(response => { return response.json() })
         .then(result => {
             res = result
-           var date = moment().format("L")
+            var date = moment().format("L")
             $('#weatherBigCard').empty();
             $('#searchHistory').append(`<button class="btn btn-sm btn-info my-1" style="max-width: fit-content"; onclick="getWeather(${res.name})">${res.name}</button>`)
             $('#weatherBigCard').append(
@@ -46,53 +46,48 @@ function getWeather(cityName){
                 <h3>5-Day Forecast</h3>
             </div>
         </div>
-        <div id="fiveDayCards" class="row">
-
-                   
+        <div id="fiveDayCards" class="row">    
         </div>`)
-
-           // console.log(res)
             return res
-            }
+        }
         ).then(res => {
             var latitude = res.coord.lat;
             var longitude = res.coord.lon;
-            getUVByCoordinates(latitude,longitude)
-            getWeatherByCoordinates(latitude,longitude)
-            
+            getUVByCoordinates(latitude, longitude)
+            getWeatherByCoordinates(latitude, longitude)
+
         }).catch(error => console.log('error', error));
 
-        
-        
-        
-        
 }
-function getUVByCoordinates(latitude,longitude){
+
+//function to get UV data
+function getUVByCoordinates(latitude, longitude) {
     var UVurl = `https://api.openweathermap.org/data/2.5/uvi?&lat=${latitude}&lon=${longitude}&appid=${apikey}`
-            fetch(
-                        UVurl, requestOptions
-                    ).then(function(response){
-                        return response.json()
-                        //Uv.text( response.value);
-                    }).then(res => {
-                        
-                        $('#uv-index').append("UV-index: " + res.value)
-                        
-                    })
-                    
+    fetch(
+        UVurl, requestOptions
+    ).then(function (response) {
+        return response.json()
+    }).then(res => {
+
+        $('#uv-index').append("UV-index: " + res.value)
+
+    })
+
 }
-function getWeatherByCoordinates(latitude,longitude){
+
+//function to get the 5 day forcast info
+function getWeatherByCoordinates(latitude, longitude) {
     var forcastUrl = `https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=${apikey}&lat=${latitude}&lon=${longitude}`
-            fetch(
-                        forcastUrl, requestOptions
-                    ).then(function(response){
-                        
-                        return response.json()
-                        
-                    }).then(res => {
-                        for (var i = 1; i < res.list.length; i+=8) {
-                            var dateCard = moment(res.list[i].dt_txt).format("L")
-                         $('#fiveDayCards').append(`
+    fetch(
+        forcastUrl, requestOptions
+    ).then(function (response) {
+
+        return response.json()
+
+    }).then(res => {
+        for (var i = 1; i < res.list.length; i += 8) {
+            var dateCard = moment(res.list[i].dt_txt).format("L")
+            $('#fiveDayCards').append(`
                          <div  class="col-2">
                          <div class="card">
                              <div class="card-body">
@@ -104,12 +99,10 @@ function getWeatherByCoordinates(latitude,longitude){
                              </div>
                          </div>
                          </div>
-                     `)   
+                     `)
 
+        }
 
-                        }
-                        console.log(res)
-                        
-                    })
-                    
+    })
+
 }
